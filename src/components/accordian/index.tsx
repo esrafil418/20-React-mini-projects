24b94a286cb71c91aaf284db806a32b889e8b4ec
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-10 13:05:28 +0330

    feat: handle multiple accordion can be open

--- src/components/accordian/index.tsx
+++ src/components/accordian/index.tsx
@@ -4,27 +4,58 @@ import data from "./data.js";
 
 export default function Accordion() {
 	const [selected, setSelected] = useState<string | null>(null);
+	const [enable, setEnable] = useState(false);
+	const [multi, setMulti] = useState<string[]>([]);
 
 	function handleSingleSelection(getCurrentId: string) {
 		setSelected(getCurrentId === selected ? null : getCurrentId);
 	}
+
+	function handleMultiSelection(getCurrentId: string) {
+		let copyMulti = [...multi];
+		const findIndexOfCurrentId = copyMulti.indexOf(getCurrentId);
+
+		if (findIndexOfCurrentId === -1) copyMulti.push(getCurrentId);
+		else copyMulti.splice(findIndexOfCurrentId, 1);
+
+		setMulti(copyMulti);
+	}
+
 	return (
 		<div className="wrapper">
+			<button
+				onClick={() => setEnable(!enable)}
+				type="button"
+				className="enable"
+			>
+				Enable Multi Selection
+			</button>
 			<div className="accordion">
 				{data && data.length > 0 ? (
 					data.map((item) => (
 						<div key={item.id} className="item">
 							<button
 								type="button"
-								onClick={() => handleSingleSelection(item.id)}
+								onClick={
+									enable
+										? () => handleMultiSelection(item.id)
+										: () => handleSingleSelection(item.id)
+								}
 								className="title"
 							>
 								<h3>{item.question}</h3>
 								<span className="plus">{selected === item.id ? "-" : "+"}</span>
 							</button>
-							{selected === item.id ? (
+							{enable
+								? multi.indexOf(item.id) !== -1 && (
+										<div className="content">{item.answer}</div>
+									)
+								: selected === item.id && (
+										<div className="content">{item.answer}</div>
+									)}
+							{/* {selected === item.id || multi.indexOf(item.id) !== -1 ? (
 								<div className="content">{item.answer}</div>
-							) : null}
+							) : null} */}
 						</div>
 					))
 				) : (
