8d0a2208455fb51814224bc26f2791a9a9b8781c
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-10 12:37:31 +0330

    feat(style): Add Style

--- src/components/accordian/index.tsx
+++ src/components/accordian/index.tsx
@@ -3,17 +3,28 @@ import "./style.css";
 import data from "./data.js";
 
 export default function Accordion() {
-	const [selected, setSelected] = useState(null);
+	const [selected, setSelected] = useState<string | null>(null);
+
+	function handleSingleSelection(getCurrentId: string) {
+		setSelected(getCurrentId === selected ? null : getCurrentId);
+	}
 	return (
 		<div className="wrapper">
 			<div className="accordion">
 				{data && data.length > 0 ? (
 					data.map((item) => (
 						<div key={item.id} className="item">
-							<div className="title">
+							<button
+								type="button"
+								onClick={() => handleSingleSelection(item.id)}
+								className="title"
+							>
 								<h3>{item.question}</h3>
-								<span>+</span>
-							</div>
+								<span className="plus">{selected === item.id ? "-" : "+"}</span>
+							</button>
+							{selected === item.id ? (
+								<div className="content">{item.answer}</div>
+							) : null}
 						</div>
 					))
 				) : (
